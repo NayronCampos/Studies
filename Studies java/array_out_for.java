859446be89664c92189607330d068ea8ba20d80b
NayronCampos <nayroncampossoares@gmail.com>
2025-03-21 16:14:58 -0300

    save

---
+++ Studies java/array_out_for.java
@@ -0,0 +1,25 @@
+import.util.Arrays;
+
+public class array_out_for{
+
+    public static void main(String[] args){
+
+        //Maneira 01 = array impresso com for.
+
+        System.out.println("Your array:");
+
+        String[] nomes;
+        nomes = new String[]{"João", "Camila", "José"};
+
+        for(int i=0; i<3; i++){
+        System.out.println("Hey " + nomes[i]);
+        }
+
+        //Maneira 02 = array impresso metodo de import.util.Arrays
+
+        String[] sabores;
+        sabores = new String[] {"Chocolate", "Menta", "Banana"}
+
+    }
+
+}

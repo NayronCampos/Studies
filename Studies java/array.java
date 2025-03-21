65a952c46ef20e782b7816bb93bcafc3b38b4fb2
NayronCampos <nayroncampossoares@gmail.com>
2025-03-21 16:03:57 -0300

    array_int

---
+++ Studies java/array.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+public class array{
+
+
+
+    public static void main(String[] args){
+
+        System.out.println("Hello world\n thats my array, he have more numbers and im print there in our vision now:\n");
+        int[] number;
+        number = new int[] {1,2,3,4,5,6};
+
+
+        System.out.println(Arrays.toString(number));
+    }
+}
